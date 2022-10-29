f910df6a53d6d0056c9e4ce437f119a0af4c4c1e
taylorgrant <taylorgrant38@gmail.com>
2022-10-28 19:49:14 -0700

    a lot of globals

---
+++ R/globals.R
@@ -0,0 +1,11 @@
+utils::globalVariables(c("creative", "info","link","media","rows",
+                         "advertiser", "brand", "parent", "product",
+                         "quarter", "row_number", "spend", "colorspace",
+                         "density", "group", "height", "width",
+                         "Format", "downloadButton", "downloadHandler",
+                         "fileInput", "fluidPage", "htmlOutput", "id", "mainPanel",
+                         "n", "reactive", "renderText", "req", "selector", "shinyApp",
+                         "sidebarLayout", "sidebarPanel", "titlePanel"
+
+
+))
