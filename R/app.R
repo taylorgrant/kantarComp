ff6d55234b7cb727581b586ae48f2e7556c802e8
taylorgrant <taylorgrant38@gmail.com>
2022-10-29 16:47:39 -0700

    added modal and new plots

--- R/app.R
+++ R/app.R
@@ -2,35 +2,47 @@ library(shiny)
 
 kantarApp <- function(...) {
 
-  ui = fluidPage(
-    titlePanel("Kantar Creative Pull"),
-    sidebarLayout(
-      sidebarPanel(
-        fileInput('file1', 'Choose xlsx file',
+  ui = shiny::fluidPage(
+    shiny::titlePanel("Kantar Creative Pull"),
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(
+        shiny::fileInput('file1', 'Choose xlsx file',
                   accept = c(".xlsx", ".xls")
         ),
-        htmlOutput("table") %>% shinycssloaders::withSpinner(color="#0dc5c1"),
-        downloadButton("report", "Generate Report")
+        shiny::htmlOutput("table") %>% shinycssloaders::withSpinner(color="#0dc5c1"),
+        shiny::downloadButton("report", "Generate Report")
       ),
-      mainPanel(
-        # htmlOutput("about")
+      shiny::mainPanel(
+        shiny::tabsetPanel(
+          shiny::tabPanel("Advertiser",
+                          highcharter::highchartOutput("advertiser", height = "450px")
+          ),
+          shiny::tabPanel("Brand",
+                          highcharter::highchartOutput("brand", height = "450px")
+          ),
+          shiny::tabPanel("Product",
+                          highcharter::highchartOutput("product", height = "450px")
+          ),
+        )
       )
     )
   )
 
   server = function(input, output, session) {
 
-    # output$about <- renderUI({HTML("<b>What is this?</b><br>This is an app that designed to ingest and process Kantar competitive
-    #                                data. The app will read in all available spend data for each advertiser, and will also scan through
-    #                                and render all digital advertising creative in the form of gifs and imagery.<br><br>
-    #                                <b>What do I need to do?</b><br>Use the browse button at left to find the Kantar file that you want to
-    #                                upload an ingest. The file should be an <b>'.xlsx'</b> and it should begin at the <b>Parent</b> level
-    #                                within Kantar's hierarchy.<br><br>
-    #                                <b>Anything else I should know?</b><br> This app can take several minutes depending how much creative is available.
-    #                                Each image is read in individually. If an advertiser is using a gif, then each image of the gif must be read in.
-    #                                <em>Please be patient.</em><br><br> When the app is finished, it will display a table with the number of assets by format type
-    #                                above the<code>Generate Report</code> button. Once you see the table, click the Report button and an HTML file will process and be downloaded to your
-    #                                computer.")})
+    shiny::showModal(shiny::modalDialog(shiny::HTML("<b>What is this?</b><br>This is an app that will ingest and process Kantar competitive
+                                   data. The app will read in all available spend data for each advertiser, and will also scan through
+                                   and render any digital banners or gifs that Kantar has identified.<br><br>
+                                   <b>What do I need to do?</b><br>Use the browse button at left to find the Kantar file that you want to
+                                   upload. The file should be an excel doc - either <b>'.xlsx'</b> or <b>'.xls'</b>. Once read in and processed,
+                                   total quarterly spend data will be calculated at the advertiser, brand, product, and media levels.<br><br>
+                                   <b>Anything else I should know?</b><br> This app can take several minutes depending on the number of creative assets.
+                                   Each image is read in individually. If an advertiser is using a gif, then each image of the gif must be read in as well.
+                                   <em>Please be patient.</em><br><br> When the data has been processed, graphs and tables will appear in the main panel. A table with the
+                                   total counts of image assets will also appear at left above the <code>Generate Report</code> button. Once you see the table,
+                                   click <code>Generate Report</code> and an HTML file will process, which you can save to your computer.<br><br>
+                                   One other thing to note is that sometimes Kantar will mess up and misidentify the ads running for a certain advertiser."),
+                               easyClose = T))
 
     # now that i have the data it has to be processed
 
@@ -40,16 +52,7 @@ kantarApp <- function(...) {
       pull_kantar(inFile$datapath)
     })
 
-    # output$table <- renderTable({
-    #   image_out() %>%
-    #     distinct(creative, .keep_all = TRUE) %>%
-    #     count(Format = format) %>%
-    #     rename(Total = n) %>%
-    #     filter(!is.na(Format))
-    #     # knitr::kable("html", caption = "Imagery Summary") %>%
-    #     # kable_styling(full_width = T)
-    # })
-
+    # summary image table
     output$table <- renderText({
       data_out()$image_out %>%
         dplyr::distinct(creative, .keep_all = TRUE) %>%
@@ -60,38 +63,21 @@ kantarApp <- function(...) {
         kableExtra::kable_styling(full_width = T)
     })
 
-    # spend_data <- reactive({
-    #   summarise_spend(image_out())
-    # })
 
-    # pull_kantar(input$file1)
-    # output$hcontainer <- renderHighchart({
-    #
-    #   spend_data <- summarise_spend(kantar_data())
-    #   hcoptslang <- getOption("highcharter.lang")
-    #   hcoptslang$thousandsSep <- ","
-    #   options(highcharter.lang = hcoptslang)
-    #
-    #   pal <- c('#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce',
-    #            '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a')
-    #
-    #   hc <- hchart(spend_data$advertiser, 'column', hcaes(x = quarter, y = spend, group = advertiser, marker = FALSE)) %>%
-    #     hc_xAxis(title = list(text = "Quarterly Advertiser Spend"),
-    #              type = "category") %>%
-    #     hc_yAxis(title = list(text = "Spend")) %>%
-    #     hc_colors(pal) %>%
-    #     hc_plotOptions(line = list(marker = list(enabled = FALSE))) %>%
-    #     hc_title(text = paste0("Spend by Quarter")) %>%
-    #     hc_credits(enabled = TRUE,
-    #                text = "Source: ",
-    #                style = list(fontSize = "10px"))
-    #
-    #
-    #
-    # })
+    # highcharter spend data
+    output$advertiser <- highcharter::renderHighchart({
+      plot_highchart(data_out()$spend, advertiser)
+    })
+    output$brand <- highcharter::renderHighchart({
+      plot_highchart(data_out()$spend, brand)
+    })
+    output$product <- highcharter::renderHighchart({
+      plot_multichart(data_out()$spend, brand, product)
+    })
 
 
-    output$report <- downloadHandler(
+
+    output$report <- shiny::downloadHandler(
       # For PDF output, change this to "report.pdf"
       filename = "report.html",
       content = function(file) {
@@ -104,9 +90,7 @@ kantarApp <- function(...) {
         file.copy("report.Rmd", tempReport, overwrite = TRUE)
 
         # Set up parameters to pass to Rmd document
-        params <- list(data = data_out()
-                       # filename = stringr::str_replace_all(file()$name, ".xlsx", ".rds")
-        )
+        params <- list(data = data_out())
 
         # Knit the document, passing in the `params` list, and eval it in a
         # child of the global environment (this isolates the code in the document
