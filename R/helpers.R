396d11c4940e4095ba4b9a240205e7db93dc47a3
taylorgrant <taylorgrant38@gmail.com>
2022-10-29 16:47:26 -0700

    adding highcharter

--- R/helpers.R
+++ R/helpers.R
@@ -45,3 +45,88 @@ add_image <- function(imgurl, size){
   magick::image_read(imgurl) %>%
     magick::image_scale(stringr::str_extract(size, "[0-9]+"))
 }
+
+#' Plot Highchart
+#' @description Function to parse spend data and plot with `highcharter`
+#' @param tbl Spend data
+#' @param group What to aggregate by - advertiser, brand, product
+#'
+#' @return highcharter graph
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' plot_highchart(spend, advertiser)
+#' }
+plot_highchart <- function(tbl, group) {
+  group <- dplyr::enexpr(group)
+  data <- tbl %>%
+    dplyr::group_by(!!group, quarter) %>%
+    dplyr::summarise(spend = sum(spend)) %>%
+    dplyr::filter(spend > 0)
+
+  # highcharter
+  hcoptslang <- getOption("highcharter.lang")
+  hcoptslang$thousandsSep <- ","
+  options(highcharter.lang = hcoptslang)
+
+  pal <- c('#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce',
+           '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a')
+
+  hc <- highcharter::hchart(data, 'column',
+                            highcharter::hcaes(x = quarter, y = spend,
+                                     group = !!group, marker = FALSE)) %>%
+    highcharter::hc_xAxis(title = list(text = glue::glue("Quarterly {stringr::str_to_title(group)} Spend")),
+             type = "category") %>%
+    highcharter::hc_yAxis(title = list(text = "Spend")) %>%
+    highcharter::hc_colors(pal) %>%
+    highcharter::hc_plotOptions(line = list(marker = list(enabled = FALSE))) %>%
+    highcharter::hc_title(text = paste0("Spend by Quarter")) %>%
+    highcharter::hc_credits(enabled = TRUE,
+               text = "Source: Kantar",
+               style = list(fontSize = "10px"))
+  return(hc)
+}
+
+#' Plot Highchart (multgroup)
+#' @description Passing multiple arguments to group by and hardcoding the plot
+#' @param tbl Spend data in tibble
+#' @param ... Multiple groups
+#'
+#' @return Highchart object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' plot_multichart(spend, brand, product)
+#' }
+plot_multichart <- function(tbl, ...) {
+  groupCol <- dplyr::quos(...)
+  data <- tbl %>%
+    dplyr::group_by(!!!groupCol, quarter) %>%
+    dplyr::summarise(spend = sum(spend)) %>%
+    dplyr::filter(spend > 0) %>%
+    tidyr::unite("product", brand:product, sep = " - ")
+
+  # highcharter
+  hcoptslang <- getOption("highcharter.lang")
+  hcoptslang$thousandsSep <- ","
+  options(highcharter.lang = hcoptslang)
+
+  pal <- c('#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce',
+           '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a')
+
+  hc <- highcharter::hchart(data, 'column',
+                            highcharter::hcaes(x = quarter, y = spend,
+                                               group = product, marker = FALSE)) %>%
+    highcharter::hc_xAxis(title = list(text = glue::glue("Quarterly Product Spend")),
+             type = "category") %>%
+    highcharter::hc_yAxis(title = list(text = "Spend")) %>%
+    highcharter::hc_colors(pal) %>%
+    highcharter::hc_plotOptions(line = list(marker = list(enabled = FALSE))) %>%
+    highcharter::hc_title(text = paste0("Spend by Quarter")) %>%
+    highcharter::hc_credits(enabled = TRUE,
+               text = "Source: Kantar",
+               style = list(fontSize = "10px"))
+  return(hc)
+}
