b2f7266d7095787f12014b730d319b8f5a50271e
taylorgrant <taylorgrant38@gmail.com>
2022-11-04 16:00:42 -0700

    adding sliderInput asset control

--- R/app.R
+++ R/app.R
@@ -6,11 +6,13 @@ kantarApp <- function(...) {
     shiny::titlePanel("Kantar Creative Pull"),
     shiny::sidebarLayout(
       shiny::sidebarPanel(
+        shiny::sliderInput("slider1", label = "Assets per advertiser", min = 0,
+                    max = 100, value = 40),
         shiny::fileInput('file1', 'Choose xlsx file',
                   accept = c(".xlsx", ".xls")
         ),
-        shiny::htmlOutput("table") %>% shinycssloaders::withSpinner(color="#0dc5c1"),
-        shiny::downloadButton("report", "Generate Report")
+      shiny::htmlOutput("table") %>% shinycssloaders::withSpinner(color="#0dc5c1"),
+      shiny::downloadButton("report", "Generate Report")
       ),
       shiny::mainPanel(
         shiny::fluidRow(
@@ -53,40 +55,47 @@ kantarApp <- function(...) {
           )
         )
       )
-    )
+  )
+
 
 
   server = function(input, output, session) {
 
-    shiny::showModal(shiny::modalDialog(shiny::HTML("<b>What is this?</b><br>This is an app that will ingest and process Kantar competitive
+    shiny::showModal(shiny::modalDialog(shiny::HTML("<p style='color:red;'><b>Important:</b> Please open the file downloaded from Kantar and resave it.
+                                                    The Kantar excel download is missing a necessary XML file. After you resave the file, it can be read in.</p><b>What is this?</b><br>This is an app that will ingest and process Kantar competitive
                                    data. The app will read in all available spend data for each advertiser, and will also scan through
-                                   and render any digital banners or gifs that Kantar has identified.<br><br>
-                                   <b>What do I need to do?</b><br>Use the browse button at left to find the Kantar file that you want to
+                                   and render up to 100 digital banners or gifs per advertiser that ran during the previous quarter.<br><br>
+                                   <b>What do I need to do?</b><br>First, select the maximum number of assets that should be rendered in the report. The assets
+                                   are sorted by spend, so whatever you choose for the slider value, the report will pull the N assets with the greatest
+                                   spend during the last quarter.<br><br>
+                                   Next, use the browse button at left to find the Kantar file that you want to
                                    upload. The file should be an excel doc - either <b>'.xlsx'</b> or <b>'.xls'</b>. Once read in and processed,
-                                   total quarterly spend data will be calculated at the advertiser, brand, product, and media levels.<br><br>
-                                   <b>Anything else I should know?</b><br> This app can take several minutes depending on the number of creative assets.
-                                   Each image is read in individually. If an advertiser is using a gif, then each image of the gif must be read in as well.
+                                   total quarterly spend data will be calculated at multiple levels -- advertiser, brand, product, and media.<br><br>
+                                   <b>Anything else I should know?</b><br> This app can take upwards of 10 minutes to run depending on the number of creative assets.
                                    <em>Please be patient.</em><br><br> When the data has been processed, graphs and tables will appear in the main panel. A table with the
                                    total counts of image assets will also appear at left above the <code>Generate Report</code> button. Once you see the table,
                                    click <code>Generate Report</code> and an HTML file will process, which you can save to your computer.<br><br>
-                                   One other thing to note is that sometimes Kantar will mess up and misidentify the ads running for a certain advertiser."),
+                                   Two more notes - first, Kantar can sometimes misattribute creative assets to advertisers; second, there is no way of de-duplicating
+                                   creative assets. It's not out of the ordinary for the same creative to be assigned multiple unique ids."),
                                easyClose = T))
 
     # now that i have the data it has to be processed
 
-    data_out <- reactive({
-      req(input$file1)
-      inFile <- input$file1
-      pull_kantar(inFile$datapath)
-    })
+      data_out <- shiny::reactive({
+        shiny::req(input$file1, input$slider1)
+        inFile <- input$file1
+        pull_kantar(inFile$datapath, input$slider1)
+      })
+
 
     # summary image table
-    output$table <- renderText({
+    output$table <- shiny::renderText({
       data_out()$image_out %>%
         dplyr::distinct(creative, .keep_all = TRUE) %>%
         dplyr::count(Format = format) %>%
         dplyr::rename(Total = n) %>%
         dplyr::filter(!is.na(Format)) %>%
+        dplyr::rename(Format = Format) %>%
         knitr::kable("html", caption = "Imagery Summary") %>%
         kableExtra::kable_styling(full_width = T)
     })
@@ -120,7 +129,7 @@ kantarApp <- function(...) {
       spend_table(data_out()$spend, "media")
     })
 
-    brandcount <- reactive({
+    brandcount <- shiny::reactive({
       length(unique(data_out()$image_out$advertiser))
     })
 
@@ -149,6 +158,7 @@ kantarApp <- function(...) {
                           params = params,
                           envir = new.env(parent = globalenv())
         )
+        unlink(c("kantar_jpgs", "tmpdata", "kantar_gifs"), recursive=TRUE)
       }
     )
 
